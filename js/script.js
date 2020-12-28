941ecea1763864320751f73b0f720110f944d9c9
Wendy C <chriscowendy@gmail.com>
2020-12-28 10:11:46 -0800

    Completed the rest of the tutorial.

--- js/script.js
+++ js/script.js
@@ -3,65 +3,113 @@ const calculator = {
     firstOperand: null,
     waitingForSecondOperand: false,
     operator: null,
-};
-
-function inputDigit(digit) {
-    const { displayValue } = calculator;
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-    console.log(calculator);
-}
-
-function inputDecimal(dot) {
-    if (!calculator.displayValue.includes(dot)) {
-        calculator.displayValue += dot;
+  };
+  
+  function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+  
+    if (waitingForSecondOperand === true) {
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperand = false;
+    } else {
+      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
     }
-}
-
-function handleOperator(nextOperator) {
-    const { firstOperand, displayValue, operator } = calculator;
+  
+    console.log(calculator);
+  }
+  
+  function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true) {
+        calculator.displayValue = '0.'
+        calculator.waitingForSecondOperand = false;
+        return;
+    }
+    if (!calculator.displayValue.includes(dot)) {
+      calculator.displayValue += dot;
+    }
+  }
+  
+  function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator
     const inputValue = parseFloat(displayValue);
 
-    if (firstOperand === null && !isNaN(inputValue)) {
-        calculator.fistOperand = inputValue;
+    if (operator && calculator.waitingForSecondOperand) {
+        calculator.operator = nextOperator;
+        console.log(calculator);
+        return;
     }
-
-    calculator.waitingforSecondOperand = true;
+  
+    if (firstOperand == null && !isNaN(inputValue)) {
+      calculator.firstOperand = inputValue;
+  
+    } else if (operator) {
+      const result = calculate(firstOperand, inputValue, operator);
+  
+      calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+      calculator.firstOperand = result;
+    }
+  
+    calculator.waitingForSecondOperand = true;
     calculator.operator = nextOperator;
     console.log(calculator);
-}
+  }
+  
+  function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+      return firstOperand + secondOperand;
+    } else if (operator === '-') {
+      return firstOperand - secondOperand;
+    } else if (operator === '*') {
+      return firstOperand * secondOperand;
+    } else if (operator === '/') {
+      return firstOperand / secondOperand;
+    }
+  
+    return secondOperand;
+  }
 
-function updateDisplay() {
+  function resetCalculator() {
+      calculator.displayValue = '0';
+      calculator.firstOperand = null;
+      calculator.waitingForSecondOperand = false;
+      calculator.operator = null;
+      console.log(calculator);
+  }
+  
+  function updateDisplay() {
     const display = document.querySelector('.calculator-screen');
     display.value = calculator.displayValue;
-}
-
-updateDisplay();
-
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
+  }
+  
+  updateDisplay();
+  
+  const keys = document.querySelector('.calculator-keys');
+  keys.addEventListener('click', (event) => {
     const { target } = event;
-
+    const { value } = target;
     if (!target.matches('button')) {
-        return;
+      return;
+    }
+    
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal(value);
+            break;
+        case 'all-clear':
+            resetCalculator();
+            break;
+        default:
+            if(Number.isInteger(parseFloat(value))) {
+                inputDigit(value);
+            }
     }
 
-    if (target.classList.contains('operator')) {
-        handleOperator(target.value);
-        updateDisplay();
-        return;
-    }
-
-    if (target.classList.contains('decimal')) {
-        inputDecimal(target.value);
-        updateDisplay();
-        return;
-    }
-
-    if(target.classList.contains('all-clear')) {
-        console.log('clear', target.value);
-        return;
-    }
-
-    inputDigit(target.value);
     updateDisplay();
-});
+  });
